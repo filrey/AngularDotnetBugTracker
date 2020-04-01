c9407213bcfdd3e890e103499f0064dd5eae242f
Filiberto Reyes <filibertoreyes55@gmail.com>
2020-03-31 20:59:13 -0700

    seeding user data to the database

--- DatingApp.API/Models/Photo.cs
+++ DatingApp.API/Models/Photo.cs
@@ -6,16 +6,18 @@ namespace DatingApp.API.Models
     {
         public int Id { get; set; }
 
-        public int Url { get; set; }
+        public string Url { get; set; }
 
 
-        public int Description { get; set; }
+        public string Description { get; set; }
 
         public DateTime DateAdded { get; set; }
 
         public bool IsMain { get; set; }
 
-        public User User { get; set; }
+        public string PublicId { get; set; }
+
+        public virtual User User { get; set; }
 
         public int UserId { get; set; }
 
